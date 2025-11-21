1017c7cf82eebb599b23681222f60ea4470c694d
Maksym Rutkovskyi <maxikrud0071@gmail.com>
2025-11-21 09:58:35 +0200

    add password hashing

--- services/usersServices.js
+++ services/usersServices.js
@@ -1,18 +1,18 @@
 import User from '../models/user.js';
+import bcrypt from 'bcrypt';
 
 async function getUserByEmail(email) {
   return await User.findOne({ where: { email: email } });
 }
 
 async function addUser(email, password) {
+  const hashedPassword = await bcrypt.hash(password, 10);
   const user = await User.create({
-    email,
-    password,
+    email, password: hashedPassword,
   });
   return { user: { email: user.email, subscription: user.subscription } };
 }
 
 export default {
-  getUserByEmail,
-  addUser,
+  getUserByEmail, addUser,
 };
